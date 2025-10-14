7e756760f67d64ed3af1dcf3e47a2e47762a9022
SammMarshall <109095378+SammMarshall@users.noreply.github.com>
2025-10-14 01:40:27 +0000

    Implementa path hack para resolver ModuleNotFoundError no deploy

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,4 +1,7 @@
 # dashboard/app.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 import streamlit as st
 from datetime import date, timedelta
